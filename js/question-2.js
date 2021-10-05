d76b5b70c5ac8874f084b2a77af8f1db8ba6d230
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-10-05 18:31:45 +0200

    update answer

--- js/question-2.js
+++ js/question-2.js
@@ -1,60 +1,68 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=2904233664034c1da06563d6fd16478c"
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=2904233664034c1da06563d6fd16478c";
 
 const resultContainer = document.querySelector(".result");
 
-async function getApi(){
-// checking for errors with try
-    try {
-        //connecting to API
-        const response = await fetch (url);
+async function getApi() {
+  // checking for errors with try
+  try {
+    //connecting to API
+    const response = await fetch(url);
 
-        const result = await response.json();
-//finding the info
-        const games = result.results;
-        
+    const result = await response.json();
+    //finding the info
+    const games = result.results;
+    //displaying loader
     resultContainer.innerHTML = "";
 
+    for (let i = 0; i < games.length; i++) {
+      //counting number of tags
+      //games[i].tags.length = counts the number of tags
 
-    for(let i = 0; i < games.length; i++){
-        
-//counting number of tags
-        const tags = games[i].tags;
+      if (i === 8) {
+        //stops after 8 has been reached
+        break;
+      }
 
-        let count = 0;
-
-        for(let i = 0; i < tags.length; i++){
-            
-            count = count+1;
-            
-        }
-        // console.log(count)
-  
-        
-//stopping the loop at 8
-        if(i === 8) {
-            break;
-        }
-
-//deploying the info        
-        resultContainer.innerHTML+= `<div class="result"> ${games[i].name} | Rating: ${games[i].rating} tags: ${count} </div>`; 
-    }
-   
-// catching error 
-    }   catch (error) {
-        console.log("error occured", error);
-        resultContainer.innerHTML = "This didnt go as planned";
+      //deploying the info
+      resultContainer.innerHTML += `<div class="result"> ${games[i].name} | Rating: ${games[i].rating} tags: ${games[i].tags.length} </div>`;
     }
 
+    // catching error
+  } catch (error) {
+    console.log("error occured", error);
+    resultContainer.innerHTML = "This didnt go as planned";
+  }
 }
 
 getApi();
 
+// sucky shit code below
+
 //catching other error
-try {
-    getApi();
-    console.log("success");
-}
-catch(error) {
-    console.log("error occured", error);
-    resultContainer.innerHTML = "This didnt go as planned";
-}
+// try {
+//   getApi();
+//   console.log("success");
+// } catch (error) {
+//   console.log("error occured", error);
+//   resultContainer.innerHTML = "This didnt go as planned";
+// }
+
+//let count = 0;
+
+// for(let i = 0; i < tags.length; i++){
+
+//     count = count+1;
+
+// }
+// console.log(count)
+// const tags = games[i].tags;
+
+// tags = 0
+
+// for(let i = 0; i < tags.length; i++){
+
+//     tags = tags+1
+
+//stopping the loop at 8
+//   const tags = games[i].tags;
